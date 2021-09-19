5aa0e2446360945a511fb8ae943b21fb1d7d7d1b
tzlan <talzana55@gmail.com>
2021-09-19 10:56:29 +0200

    Test 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world test 1.";
     }
 
     public static void main(String[] args) {
